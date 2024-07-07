97fc70ae7b1a6b347a65231d74f793b43023125e
FelixFoxHD <felixfoxhd@gmail.com>
2024-07-07 12:01:19 +0200

    Initial commit

---
+++ src/main/java/dev/felixfoxxy/openop/OpenOP.java
@@ -0,0 +1,17 @@
+package dev.felixfoxxy.openop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OpenOP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
